881ec207013c99139277fa1333ffa82508471116
cazterk <zcephas2@gmail.com>
2025-02-15 17:24:51 +0200

    dockerized server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -9,7 +10,14 @@ plugins {
     kotlin("jvm") version "2.1.10"
     id("io.ktor.plugin") version "2.2.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))  // Specify Java 21
+    }
 }
 
 group = "app.netlify.cazterk"
@@ -54,4 +62,18 @@ dependencies {
     sshAntTask("org.apache.ant:ant-jsch:1.10.12")
 
 }
+
+//tasks.register<Jar>("buildFatJar") {
+//    archiveBaseName.set("meetApp-ktor-backend")
+//    archiveVersion.set("")
+//    archiveClassifier.set("")
+//
+//    manifest {
+//        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+//    }
+//
+//    from(sourceSets.main.get().output)
+//
+//    dependsOn("shadowJar")
+//}
 
